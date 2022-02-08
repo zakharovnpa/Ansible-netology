31097ffaa2fc7a65a04c804d5e8ec50a331ad909
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2022-02-08 13:47:49 +0400

    Update rundock.sh

--- rundock.sh
+++ rundock.sh
@@ -2,23 +2,19 @@
 #
 # Скрипт для запуска контейнеров docker
 
-docker run -d --rm --name centos7 eeb6ee3f44bd sleep 60000000 && 
 
-docker run -d --rm --name fedore c31790496329 sleep 60000000 &&
+docker run -d --rm --name centos7 eeb6ee3f44bd sleep 60000000 && 	# Старт нового контейнера с именем centos7 на основе образа eeb6ee3f44bd
+docker run -d --rm --name fedore c31790496329 sleep 60000000 &&		# Аналогично для fedore
+docker run -d --rm --name ubuntu 42a4e3b21923 sleep 60000000 &&		# Аналогично для ubuntu
 
-docker run -d --rm --name ubuntu 42a4e3b21923 sleep 60000000 &&
+	docker ps | grep 'Up' && 		# Вывод на терминал контейнеров, которые запустились
+	sleep 10 &&				# Выполнить остановку скрипта на 10 секунд
 
-	docker ps | grep 'Up' && 
-	sleep 10 &&
+ansible-playbook -i inventory/prod.yml site.yml --ask-vault-pass >> ansible.log && 	# Запуск playbook с запросом пароля на расшифровку файлов 
+											# и запись результатов в файл ansible.log
 
-ansible-playbook -i inventory/prod.yml site.yml --ask-vault-pass >> ansible.log && 
+docker stop centos7 &&		# Остановка и удаление уонтейнера с именем centos7
+docker stop fedore &&		# Аналогично для fedore
+docker stop ubuntu &&		# Аналогично для ubuntu
 
-docker stop centos7 &&
-docker stop fedore &&
-docker stop ubuntu &&
-
-docker ps
-
-#sleep 20
-
-#docker ps
+docker ps		# Вывод на терминал контейнеров, которые запустились
