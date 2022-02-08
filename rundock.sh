12d9008f1530631263eb429e4712a4c372dcc56c
zakharovnpa <89311498+zakharovnpa@users.noreply.github.com>
2022-02-08 16:49:51 +0400

    Update rundock.sh

--- rundock.sh
+++ rundock.sh
@@ -2,11 +2,12 @@
 #
 # Скрипт для запуска контейнеров docker и запуска ansible-playbook
 
-
+date >> ansible.log &&		# Запись даты выполнения скрипта
 docker run -d --rm --name centos7 eeb6ee3f44bd sleep 60000000 && 	# Старт нового контейнера с именем centos7 на основе образа eeb6ee3f44bd
 docker run -d --rm --name fedore c31790496329 sleep 60000000 &&		# Аналогично для fedore
 docker run -d --rm --name ubuntu 42a4e3b21923 sleep 60000000 &&		# Аналогично для ubuntu
 
+	sleep 3 &&				# Выполнить остановку скрипта на 3 секунды
 	docker ps | grep 'Up' && 		# Вывод на терминал контейнеров, которые запустились
 	sleep 10 &&				# Выполнить остановку скрипта на 10 секунд
 
